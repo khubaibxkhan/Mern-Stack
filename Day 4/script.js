dfb1ed052fe875bd036737243fc14ea914cb84cd
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-05 22:45:08 +0530

    question 2 of day 4 task is done

--- Day 4/script.js
+++ Day 4/script.js
@@ -4,3 +4,15 @@ function add(a,b) {
     const sum = a + b;
     return sum;
 }
+
+//2. A function that checks if a number is even or odd
+
+function evenodd(a){
+    if(a%2==0){
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+
