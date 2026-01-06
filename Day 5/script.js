45a3c295fc0036eb36a621fcc1be289cc121b73c
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-06 23:18:10 +0530

    question 5 ddone for day 5 task

--- Day 5/script.js
+++ Day 5/script.js
@@ -30,4 +30,10 @@ const student = {
 
 student.skills.push("Next JS");
 
+//5. Use a loop to print all keys and values of the object
 
+const keys = Object.keys(student);
+
+for (let i = 0; i < keys.length; i++) {
+  console.log(keys[i] + " : " + student[keys[i]]);
+}
