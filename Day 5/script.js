7963ba24e796a36ccef890755cc0931b38fae789
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-06 21:50:58 +0530

    question 1 of day 4 task is done

---
+++ Day 5/script.js
@@ -0,0 +1,14 @@
+//1.A loop that prints numbers from 1 to 50 and prints "even" for even numbers.
+
+for(let i=0; i<=50 ; i++){
+    if(i%2 === 0){
+        console.log(i, " Even")
+    }else{
+        console.log(i);    
+    }
+}
+
+//2. An array of numbers and a function that returns only even numbers using filter
+//3. An object student with name, age, and skills (array)
+//4. Add a new skill to the object
+//5. Use a loop to print all keys and values of the object
