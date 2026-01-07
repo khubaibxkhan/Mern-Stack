6ace62edc90aa497c44b7a1f4043c67eeb9ed288
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-07 23:31:20 +0530

    adding click event to button

--- Day 6/script.js
+++ Day 6/script.js
@@ -6,3 +6,9 @@ paragraph.textContent = "This text is changed using JavaScript";
 
 div.style.backgroundColor = "lightgray";
 
+button.addEventListener("click" , () => {
+    const newParagraph = document.createElement("p");
+    newParagraph.textContent = "Hello this is me Khubaib welcome to day 6 of my mern stack journey";
+    div.appendChild(newParagraph);
+});
+
