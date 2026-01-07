c3bb421246f9a281ca7e2e319677f8c6676074b2
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-07 23:20:52 +0530

    changing text using java script

---
+++ Day 6/script.js
@@ -0,0 +1,5 @@
+const paragraph = document.querySelector("p");
+const button = document.querySelector("button");
+const div = document.querySelector("div");
+
+paragraph.textContent = "This text is changed using JavaScript";
