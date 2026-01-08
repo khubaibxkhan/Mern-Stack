28336501e1bf51b7931c399bf2741f03b6f241ec
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-08 18:13:18 +0530

    done with making dark mode website toggle

--- Practice/DOM 1/script.js
+++ Practice/DOM 1/script.js
@@ -1,1 +1,19 @@
 //create a toggle button that changes the screen to dark-mode when clicked & light-mode when clicked again
+
+const modeBtn = document.querySelector("#mode");
+
+let mode = "light";
+
+modeBtn.addEventListener("click" , () => {
+    if(mode === "light"){
+        mode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+        document.querySelector("h1").style.color = "white";
+        document.querySelector("p").style.color = "white";
+    }else{
+        mode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+        document.querySelector("h1").style.color = "black";
+        document.querySelector("p").style.color = "black";
+    }
+});
