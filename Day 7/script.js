30bf59b673fedc819296881cbd595b4dd381ad3f
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-08 23:59:54 +0530

    done with the scripting

--- Day 7/script.js
+++ Day 7/script.js
@@ -3,11 +3,13 @@ const input = document.querySelector('task-input');
 const taskList = document.querySelector('task-list');
 
 addBtn.addEventListener('click', () => {
-    const taskText = input.ariaValueMax.trim();
+    const taskText = input.value.trim();
 
     const li = document.createElement('li');
     const span = document.createElement('span');
     const delBtn = document.createElement('button');
+
+    span.textContent = taskText;
 
 
     delBtn.addEventListener('click', () => {
@@ -16,7 +18,8 @@ addBtn.addEventListener('click', () => {
 
     li.appendChild('span');
     li.appendChild('delBtn');
-    taskList.appendChild('li')
+    taskList.appendChild('li');
 
+    taskText.value = "";
 
 });
