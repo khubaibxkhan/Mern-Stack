ec742d18c0dac4c088ef11d1976c95b4ccf7e969
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-09 16:12:03 +0530

    compliting thet code

--- Practice/3. Event Bubble -DOM/script.js
+++ Practice/3. Event Bubble -DOM/script.js
@@ -1,8 +1,8 @@
 const musicList = document.querySelector('music-list');
 
-musicList.addEventListener('click', (e) => {
+musicList.addEventListener('click', function(e){
     if(e.target.className == 'delete'){
         const li = e.target.parentElement;
         musicList.removeChild(li);
     }
-})
+});
