b206cd3aa27b4efa9f716ac0a5f17100b39d738a
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-09 16:26:10 +0530

    making my first react project as day 8 task

---
+++ Day 8/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione cupiditate temporibus inventore at nulla atque, in eveniet aperiam voluptas veritatis maxime ex placeat rerum distinctio et est repudiandae? Ducimus velit corrupti doloremque officiis accusantium id, consequuntur sint ab esse laborum! Nobis eius, vitae officia facere molestias natus debitis inventore voluptas delectus, eos a! Molestias, explicabo? Porro saepe dignissimos iure necessitatibus.</p>
+    </>
+  )
+}
+
+export default App
