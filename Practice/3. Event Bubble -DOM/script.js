75438ca2e127bdb423f55976b82cf6dfbcdcb1c4
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-09 16:03:03 +0530

    script to the file

--- Practice/3. Event Bubble -DOM/script.js
+++ Practice/3. Event Bubble -DOM/script.js
@@ -1,7 +1,7 @@
 const musicList = document.querySelector('music-list');
 
 musicList.addEventListener('click', (e) => {
-    if(e.target.className == "delete"){
+    if(e.target.className == 'delete'){
         const li = e.target.parentElement;
         musicList.removeChild(li);
     }
