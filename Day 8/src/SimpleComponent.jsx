8ea6074f7a86047a0d853a843426ad518b2a7d6c
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-10 23:30:21 +0530

    editing the day 8 task and editing components

---
+++ Day 8/src/SimpleComponent.jsx
@@ -0,0 +1,8 @@
+export const SimpleComponent = () => {
+    return(
+        <div>
+        <h1>Hello World</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione cupiditate temporibus inventore at nulla atque, in eveniet aperiam voluptas veritatis maxime ex placeat rerum distinctio et est repudiandae? Ducimus velit corrupti doloremque officiis accusantium id, consequuntur sint ab esse laborum! Nobis eius, vitae officia facere molestias natus debitis inventore voluptas delectus, eos a! Molestias, explicabo? Porro saepe dignissimos iure necessitatibus.</p>
+      </div>
+    );
+};
