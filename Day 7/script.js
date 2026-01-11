53106a93dcc680c65560867e76b77f5ddec4808e
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-11 15:06:28 +0530

    adding 'Enter' key to add the tasks in my task manager

--- Day 7/script.js
+++ Day 7/script.js
@@ -3,14 +3,18 @@ const input = document.querySelector('#task-input');
 const taskList = document.querySelector('#task-list');
 
 addBtn.addEventListener('click', () => {
+
     const taskText = input.value.trim();
+    if (taskText === "") return;
 
     const li = document.createElement('li');
     const span = document.createElement('span');
     const delBtn = document.createElement('button');
 
+    delBtn.className = "delete-btn";
+
     span.textContent = taskText;
-     delBtn.textContent = "Delete";
+    delBtn.textContent = "Delete";
 
 
     delBtn.addEventListener('click', () => {
@@ -24,3 +28,9 @@ addBtn.addEventListener('click', () => {
     input.value = "";
 
 });
+
+input.addEventListener('keydown', (e) =>{
+    if (e.key === "Enter"){
+        addBtn.click();
+    }
+})
