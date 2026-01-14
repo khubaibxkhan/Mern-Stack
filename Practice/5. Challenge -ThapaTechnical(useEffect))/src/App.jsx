cc8eb431860dd25607a9df6367a87b6a7f0f710c
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-14 18:59:18 +0530

    completing the challenge based on useEffect

---
+++ Practice/5. Challenge -ThapaTechnical(useEffect))/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { EffectChallenge } from './components/EffectChallenge'
+
+function App() {
+  return (
+    <>
+     <EffectChallenge />
+    </>
+  )
+}
+
+export default App
---
+++ Practice/5. Challenge -ThapaTechnical(useEffect))/src/components/EffectChallenge.jsx
@@ -0,0 +1,43 @@
+// Description: In this challenge, you'll create a React component that dynamically manages state
+// using 'useState' and 'useEffect' hooks. Your component will include:
+
+// 1. A counter that increments when a button is clicked.
+// 2. An input field where users can type their name.
+// 3. The document title will update to show the current count.
+
+import { useState , useEffect } from 'react'
+export const EffectChallenge = () => {
+    const [count , setCount] = useState(0);
+    const [name , setName] = useState("");
+
+    useEffect(() => {
+        document.title = `Count : ${count}`;
+    })
+
+    const userName = (e) => {
+        setName(e.target.value);
+    }
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+  return (
+    <div>
+      <h1>Use-Effect</h1>
+
+      <p>
+        Count : <span>{count}</span>
+      </p>
+
+      <button onClick={increment}>increment </button>
+
+      <p>
+        Name : <span>{name}</span> 
+      </p>
+
+      <input type="text" onChange={userName} />
+    </div>
+  );
+};
+
