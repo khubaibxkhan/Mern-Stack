0e7122b981c490fa4037cc531bbc24eca6dbbc47
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-15 23:20:08 +0530

    adding hooks for the project

---
+++ Day-12/src/components/UserList.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const UserList = () => {
+    const [ user , setUser ] = useState([]);
+    const [ error , setError ] = useState(null);
+    const [ loading , setLoading ] = useState(true);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserList
