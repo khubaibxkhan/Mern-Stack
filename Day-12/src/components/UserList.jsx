98d6bdf0688567e246d5e1ae4399cb505b453e34
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-15 23:27:43 +0530

    styling the componets

--- Day-12/src/components/UserList.jsx
+++ Day-12/src/components/UserList.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useEffect , useState } from 'react'
 
 const UserList = () => {
     const [ user , setUser ] = useState([]);
     const [ error , setError ] = useState(null);
     const [ loading , setLoading ] = useState(true);
+
+    const API = "https://jsonplaceholder.typicode.com/users";
+
+    const fetchUser = async() => {
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+        } catch (error) {
+            
+        }
+    }
   return (
     <div>
       
