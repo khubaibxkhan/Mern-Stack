cf21395c4d9f686ff7ca29bb1c5e0b2b22104c7c
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-16 22:26:33 +0530

    mini project done -search-bar to search user data -fetching user data via public api -used parential divs to liftup the card

--- Day-14 (mini-project))/src/components/Card.jsx
+++ Day-14 (mini-project))/src/components/Card.jsx
@@ -3,7 +3,7 @@ export const Card = ({name , username , email , phone , company}) => {
   return (
     <div className="card">
         <h2>{name}</h2>
-        <p className="username"><span>username: </span>{username}</p>
+        <p className="username"><span>Username: </span>{username}</p>
         <p><span>Email: </span>{email}</p>
         <p><span>Phone: </span>{phone}</p>
         <p><span>Company: </span>{company}</p>
