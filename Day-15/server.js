b4ba0f18b8996bfe7673ae5fc9ac73e5b2da2147
khubaixkhan <khubaibxkhan22@gmail.com>
2026-01-17 23:57:47 +0530

    task of day 15 completed

--- Day-15/server.js
+++ Day-15/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const data = require('./views/data.json');
 
 const app = express();
-
-app.listen(3000);
 
 app.get('/',(req,res) => {
     res.sendFile('./views/index.html',{root: __dirname});
 });
 
+app.get('/api/hello', (req,res) => {
+    res.json(data);
+});
+
 app.use((req,res) => {
     res.status(404).sendFile('./views/404.html' , {root: __dirname});
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
 
